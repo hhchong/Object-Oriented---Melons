e3fb651a9f8f692101ac6cb2904970e7eabd7d69
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-11 16:09:01 -0700

    adjusted get_total for christmas melons and fee

--- melons.py
+++ melons.py
@@ -1,17 +1,26 @@
 """Classes for melon orders."""
-class AbstractMelonOrder:
+class AbstractMelonOrder():
 
-    def __init__(self, species, qty):
+    def __init__(self, species, qty, order_type):
 
         self.species = species
         self.qty = qty
         self.shipped = False
+        self.order_type = order_type
+
 
     def get_total(self):
         """Calculate price, including tax."""
 
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price + self.flat_fee
+        
+        if self.species == "Christmas":
+            base_price = base_price * 1.5 
+
+        total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            return total + 3
 
         return total
 
@@ -24,26 +33,24 @@ class AbstractMelonOrder:
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
-    # order_type = "domestic"
+    def __init__(self, species, qty):
+        super().__init__(species, qty, "domestic")
+
+    
     tax = 0.08
-    flat_fee = 0
+
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
+
     """An international (non-US) melon order."""
-    tax = 0.17
 
-    if qty < 10:
-        flat_fee = 3
-    else:
-        flat_fee = 0
-    # order_type = "international"
-
-
-    def __init__(self, country_code):
-        """Initialize melon order attributes."""
-
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty, "international")
         self.country_code = country_code
+
+
+    tax = 0.17
 
 
     def get_country_code(self):
